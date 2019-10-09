50c2394dc011f5d07848fdc02d23dc7b1d754b80
jiang <527813266@qq.com>
2019-10-09 17:48:47 +0800

    姜丹登录注册图片合并

--- client/src/js/loadding-StarCraft.js
+++ client/src/js/loadding-StarCraft.js
@@ -53,17 +53,40 @@ export function loaddingFlash() {
 };
 export function loaddingFooter() {
     return new Promise((resolve, reject) => {
-        $.ajax({
-            url: `${BASE_URL}/StarCraft/images/StarCraft?kind=footer`,
-            success(imageFooter) {
-                let htmlStr = "";
-                htmlStr +=
-                    `  <img src="${BASE_URL}/images/StarCraft/footer-logo-1.png">
-                       <img src="${BASE_URL}/images/StarCraft/footer-logo-2.png">
+        let htmlStr = ` <img src="${BASE_URL}/images/StarCraft/footer-logo-1.png">
+                        <img src="${BASE_URL}/images/StarCraft/footer-logo-2.png">
+                         `;
+        $(".footer-logo").html(htmlStr);
+        resolve();
+    })
+};
+export function loaddingSmm() {
+    return new Promise((resolve, reject) => {
+        let htmlStr = ` <img src="${BASE_URL}/images/StarCraft/footer-imm-1.png"><img src="${BASE_URL}/images/StarCraft/footer-imm-2.png"><img src="${BASE_URL}/images/StarCraft/footer-imm-3.png">
+        <img src="${BASE_URL}/images/StarCraft/footer-imm-4.png">
+        <img src="${BASE_URL}/images/StarCraft/footer-imm-5.png"><span>|适龄提示：适合13岁及以上使用 家长监护工程</span>
                         `;
-                $(".footer-logo").html(htmlStr);
-                resolve();
-            }
-        })
+        $(".footer-smm").html(htmlStr);
+        resolve();
+    })
+};
+export function loaddingVideo() {
+    return new Promise((resolve, reject) => {
+        let htmlStr = ` 
+            <li style="background-image: url(${BASE_URL}/images/StarCraft/landing.png);">
+                <p class="title">《星际争霸II》免费畅玩：战役</p>
+                <p class="video-btn" style="background-image: url(${BASE_URL}/images/StarCraft/imgSm_1.jpg);"><span><i class="iconfont">&#xe620;</i></span></p>
+            </li>
+            <li style="background-image: url(${BASE_URL}/images/StarCraft/landing.png);">
+                <p class="title">《星际争霸II》免费畅玩：对决</p>
+                <p class="video-btn" style="background-image: url(${BASE_URL}/images/StarCraft/imgSm_2.jpg);"><span><i class="iconfont">&#xe620;</i></span></p>
+            </li>
+            <li style="background-image: url(${BASE_URL}/images/StarCraft/landing.png);">
+                <p class="title">《星际争霸II》免费畅玩：合作任务</p>
+                <p class="video-btn" style="background-image: url(${BASE_URL}/images/StarCraft/imgSm_3.jpg);"><span><i class="iconfont">&#xe620;</i></span></p>
+            </li>
+                         `;
+        $(".swiper-grid").html(htmlStr);
+        resolve();
     })
 };
