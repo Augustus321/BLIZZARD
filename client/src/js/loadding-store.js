94214f8e5c71b6cd082212fd97134530530332f9
Augustus321 <10107928227@qq.com>
2019-10-10 22:20:00 +0800

    更新资源

--- client/src/js/loadding-store.js
+++ client/src/js/loadding-store.js
@@ -4,18 +4,24 @@ import { BASE_URL } from "./lib";
 export function loaddingFlash() {
     return new Promise((resolve, reject) => {
         $.ajax({
-            url: `${BASE_URL}/store/Banners?kind=Banner`,
+            url: `${BASE_URL}/store/banners?kind=banner`,
             success(res) {
                 console.log(res);
             let {BannerAll} = res;
             let BannerAllHtmlStr = "";
             $.each(BannerAll, (_, obj) => {
-                iconHtmlStr += `<div class="swiper-slide">
-                                    <img src="${BASE_URL}/images/store/${obj.imgGame}" alt="">
-                            </div>
-                </figure>`;
+                BannerAllHtmlStr += `
+                            <div class="swiper-slide">
+                                <div class="swiper-box"  style="background:url(${BASE_URL}/images/store/${obj.bgImg}) no-repeat;">
+                                    <div class="swiper-po">
+                                        <img src="${BASE_URL}/images/store/${obj.imgGame}" alt="" class="swiper-image">
+                                        <h5 class="swiper-text">${obj.bannerText}</h5>
+                                        <a href="javascript:;" class="swiper-link">立即购买</a>
+                                    </div>
+                                </div>
+                            </div>`;
             });
-                $(".swiper-wrapper").html(htmlStr);
+                $(".swiper-wrapper").html(BannerAllHtmlStr);
                 resolve();
             }
         })
